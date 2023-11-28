c69592bd169c8a10e220a8a048e8be1d221e65e4
Tamás B. Nemes <tb.nemes@gmail.com>
2023-11-28 10:05:58 +0100

    Calendar centralization

--- ExamTool/src/app/form/form.component.ts
+++ ExamTool/src/app/form/form.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Exam, Exams } from '../../assets/placeholder_exams';
+import { CalendarService } from '../Objects/calendar.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { EnvVars } from '../Env';
 import { Statics } from '../Statics';
 import { ExamSchedule } from '../Objects/ObjectExamenWeek';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss', '../calendar/calendar.component.scss'],
+  styleUrls: ['./form.component.scss', '../Objects/calendar.scss'],
 })
 export class FormComponent implements OnInit {
   ExamenInplannen = {
@@ -26,12 +27,14 @@ export class FormComponent implements OnInit {
   klasses: any;
   lokaal: any;
 
-  Exams = Exams;
   Image: string = '';
   ResponseData: ExamSchedule[] | undefined;
 
-  constructor(private http: HttpClient) {
-    this.manipulate();
+  constructor(
+    private http: HttpClient,
+    public calendarService: CalendarService,
+    private snackBar: MatSnackBar
+  ) {
     let header = new HttpHeaders({
       Authorization: `Bearer ${Statics.Token}`,
     });
@@ -67,25 +70,9 @@ export class FormComponent implements OnInit {
       });
   }
 
-  date: any = new Date();
-  year: number = this.date.getFullYear();
-  month: number = this.date.getMonth();
-
-  GetExameForDate(date: number, dateClass: string) {
-    let DateNew = new Date(this.year, this.month, date);
-    return this.ResponseData?.filter((examens) => {
-      if (
-        examens.agendaItem.tijd_Begin.split('T')[0] ===
-          DateNew.toISOString().split('T')[0] &&
-        dateClass != 'inactive'
-      )
-        return examens;
-
-      return null;
-    });
+  ngOnInit(): void {
+    this.calendarService.manipulate();
   }
-
-  ngOnInit(): void {}
 
   save() {
     let header = new HttpHeaders({
@@ -99,6 +86,7 @@ export class FormComponent implements OnInit {
         headers: header,
       })
       .subscribe();
+    this.notifyUser('Successvol opgeslagen!');
   }
 
   AddPopup: boolean = false;
@@ -139,6 +127,7 @@ export class FormComponent implements OnInit {
 
     this.AddPopup = false;
     this.AddClass = false;
+    this.notifyUser('Successvol opgeslagen!');
   }
 
   RemoveClass() {
@@ -154,6 +143,7 @@ export class FormComponent implements OnInit {
         body: this.ExamenInplannen.klas_Id,
       })
       .subscribe();
+    this.notifyUser('Successvol verwijderd!');
   }
 
   SaveClassRoom() {
@@ -173,6 +163,7 @@ export class FormComponent implements OnInit {
 
     this.AddPopup = false;
     this.AddClassRoom = false;
+    this.notifyUser('Successvol opgeslagen!');
   }
 
   CancelAdding() {
@@ -181,102 +172,9 @@ export class FormComponent implements OnInit {
     this.AddClass = false;
   }
 
-  months = [
-    'Januari',
-    'Februari',
-    'Maart',
-    'April',
-    'Mei',
-    'Juni',
-    'Juli',
-    'Augustus',
-    'September',
-    'Oktober',
-    'November',
-    'December',
-  ];
-
-  datum: any = '';
-  dates: { value: number; class: string }[] = [];
-
-  // Function to generate the calendar
-  manipulate = () => {
-    this.dates = [];
-
-    // Get the first day of the month
-    let dayone = new Date(this.year, this.month, 1).getDay();
-    // Get the last date of the month
-    let lastdate = new Date(this.year, this.month + 1, 0).getDate();
-    // Get the day of the last date of the month
-    let dayend = new Date(this.year, this.month, lastdate).getDay();
-    // Get the last date of the previous month
-    let monthlastdate = new Date(this.year, this.month, 0).getDate();
-    // Variable to store the generated calendar HTML
-
-    // Loop to add the last dates of the previous month
-    for (let i = dayone; i > 0; i--) {
-      this.dates.push({ value: monthlastdate - i + 1, class: 'inactive' });
-    }
-
-    // Loop to add the dates of the current month
-    for (let i = 1; i <= lastdate; i++) {
-      // Check if the current date is today
-      let isToday =
-        i === this.date.getDate() &&
-        this.month === new Date().getMonth() &&
-        this.year === new Date().getFullYear()
-          ? 'active'
-          : '';
-      this.dates.push({ value: i, class: isToday });
-    }
-
-    for (let i = dayend; i < 6; i++) {
-      this.dates.push({ value: i - dayend + 1, class: 'inactive' });
-    }
-    console.log(this.dates);
-  };
-
-  prevMonth() {
-    this.month -= 1;
-    if (this.month < 0 || this.month > 11) {
-      // Set the date to the first day of the
-      // month with the new year
-      this.date = new Date(this.year, this.month, new Date().getDate());
-
-      // Set the year to the new year
-      this.year = this.date.getFullYear();
-
-      // Set the month to the new month
-      this.month = this.date.getMonth();
-    } else {
-      // Set the date to the current date
-      this.date = new Date();
-    }
-
-    // Call the manipulate function to
-    // update the calendar display
-    this.manipulate();
-  }
-
-  nextMonth() {
-    this.month += 1;
-    if (this.month < 0 || this.month > 11) {
-      // Set the date to the first day of the
-      // month with the new year
-      this.date = new Date(this.year, this.month, new Date().getDate());
-
-      // Set the year to the new year
-      this.year = this.date.getFullYear();
-
-      // Set the month to the new month
-      this.month = this.date.getMonth();
-    } else {
-      // Set the date to the current date
-      this.date = new Date();
-    }
-
-    // Call the manipulate function to
-    // update the calendar display
-    this.manipulate();
+  notifyUser(message: string) {
+    this.snackBar.open(message, 'Ok', {
+      duration: 3000,
+    });
   }
 }
